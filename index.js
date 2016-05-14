6a784092d9e1e60b705112fb4b8281dfffb3be0a
huangxiaodong599 <huangxiaodong599@jk.cn>
2016-05-14 19:30:19 +0800

    save

--- index.js
+++ index.js
@@ -9,16 +9,29 @@ Service.prototype.getByTitle = function (columnTitle, row) {
   var sheet    = this.xlsObject[0].data;
   var titleIdx = this.titleIdx;
   var colIdx   = sheet[titleIdx].indexOf(columnTitle);
-  var row = sheet[row];
-  if(!row){
-    return "";
+  var row      = sheet[row];
+  if (!row) {
+    return null;
   }
   var content = row[colIdx];
-  if(typeof(content) == "String" ){
+  if (typeof(content) == "String") {
     return content.trim();
-  }else{
+  } else if (content == "-") {
+    return "";
+  } else {
     return content;
   }
-
 }
-module.exports               = Service;
+
+Service.prototype.getLength = function () {
+  var sheet = this.xlsObject[0].data;
+  for (i = 0; i < sheet.length; i++) {
+    var row = sheet[i];
+    if (row == undefined || row.length == 0) {
+      return i;
+    }
+  }
+  return sheet.length;
+}
+
+module.exports = Service;
